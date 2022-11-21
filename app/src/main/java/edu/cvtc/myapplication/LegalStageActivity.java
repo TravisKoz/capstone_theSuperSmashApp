75bc8732cebfe4f17ee3e5e24574024bfe5e79ef
tquinn6 <tquinn6@student.cvtc.edu>
2022-11-21 02:38:44 -0600

    Post-heart attack: Fully prepared LegalStageActivity [finishes #183753815]

---
+++ app/src/main/java/edu/cvtc/myapplication/LegalStageActivity.java
@@ -0,0 +1,14 @@
+package edu.cvtc.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LegalStageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legal_stage);
+    }
+}
