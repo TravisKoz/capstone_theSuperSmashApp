293b00f04f2f52c1d0d2526e475338ebcc98cd43
tquinn6 <tquinn6@student.cvtc.edu>
2022-12-18 23:29:56 -0600

    Added stage information to LegalStageActivity [finishes #183868599]

--- app/src/main/java/edu/cvtc/myapplication/LegalStageActivity.java
+++ app/src/main/java/edu/cvtc/myapplication/LegalStageActivity.java
@@ -6,9 +6,10 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class LegalStageActivity extends AppCompatActivity implements View.OnClickListener{
+public class LegalStageActivity extends AppCompatActivity {
 
 
 
@@ -17,83 +18,111 @@ public class LegalStageActivity extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_legal_stage);
 
-
+        // Create textviews
+        TextView starter = findViewById(R.id.stage_info_text_starter);
+        starter.setText("");
+        TextView counterpickStarter = findViewById(R.id.stage_info_counterpick_starter);
+        counterpickStarter.setText("");
+        TextView counterpick = findViewById(R.id.stage_info_text_counterpick);
+        counterpick.setText("");
 
         // Create buttons and onClickListeners
         ImageButton finalDestination = findViewById(R.id.final_destination);
-        finalDestination.setOnClickListener(this);
         ImageButton battlefield = findViewById(R.id.battlefield);
-        battlefield.setOnClickListener(this);
-        ImageButton smashville = findViewById(R.id.final_destination);
-        smashville.setOnClickListener(this);
-        ImageButton townAndCity = findViewById(R.id.final_destination);
-        townAndCity.setOnClickListener(this);
-        ImageButton smallBattlefield = findViewById(R.id.final_destination);
-        smallBattlefield.setOnClickListener(this);
-        ImageButton lylatSystem = findViewById(R.id.final_destination);
-        lylatSystem.setOnClickListener(this);
-        ImageButton pokemonStadium2 = findViewById(R.id.final_destination);
-        pokemonStadium2.setOnClickListener(this);
-        ImageButton kalosPokemonLeague = findViewById(R.id.final_destination);
-        kalosPokemonLeague.setOnClickListener(this);
-        ImageButton hollowBastion = findViewById(R.id.final_destination);
-        hollowBastion.setOnClickListener(this);
-        ImageButton yoshisStory = findViewById(R.id.final_destination);
-        yoshisStory.setOnClickListener(this);
+        ImageButton smashville = findViewById(R.id.smashville);
+        ImageButton townAndCity = findViewById(R.id.town_and_city);
+        ImageButton smallBattlefield = findViewById(R.id.small_battlefield);
+        ImageButton lylatSystem = findViewById(R.id.lylat_cruise);
+        ImageButton pokemonStadium2 = findViewById(R.id.pokemon_stadium_2);
+        ImageButton kalosPokemonLeague = findViewById(R.id.kalos_pokemon_league);
+        ImageButton hollowBastion = findViewById(R.id.hollow_bastion);
+        ImageButton yoshisStory = findViewById(R.id.yoshis_story);
 
-//        Bundle bundle = new Bundle();
-//
-//        if (findViewById(R.id.final_destination).isPressed()) {
-//
-//            bundle.putString("stageInfoKey", "The stage is a single platform and does not disrupt gameplay");
-//
-//
-//        } else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        } else if (findViewById(R.id.smashville).isPressed()) {
-//
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//        bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }  else if(findViewById(R.id.battlefield).isPressed()) {
-//            bundle.putString("stageInfoKey","Same as Final Destination, but with three added platforms");
-//
-//        }
-//
-//            if (savedInstanceState == null) {
-//                getSupportFragmentManager().beginTransaction()
-//                        .setReorderingAllowed(true)
-//                        .replace(R.id.legal_stage_container_view, LegalStageFragment.class, bundle)
-//                        .commit();
-//
-//        }
 
+
+        finalDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                starter.setText("Basic stage layout, no platforms, flat.");
+
+            }
+        });
+
+        battlefield.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                starter.setText("Same as Final Destination, with three added platforms.");
+
+            }
+        });
+
+       smashville.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               starter.setText("Same as Final Destination, with one moving platform");
+           }
+       });
+
+        townAndCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpickStarter.setText("While in early years of Ultimate the stage was consistently considered a starter[2], views of the stage have changed as more competitively viable stages (including competitively viable Hazards On stages) were added to the game. This is mainly due to Town and City usually being the only transformative stage in most competitive stage lists, as well as having a transformation which is duplicative of Final Destination.");
+            }
+        });
+
+
+
+        smallBattlefield.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpickStarter.setText("Stage layout is similar to Pokémon Stadium 2, albeit with a smaller stage size and larger platforms. Thus, there has been some debate on where Small Battlefield stands in the current stage list, with some tournaments having it replace PS2 as a starter, while others put it as a counterpick.");
+
+            }
+        });
+
+
+
+        lylatSystem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpickStarter.setText("Slants can alter gameplay; certain recoveries had difficulties grabbing the ledge prior to 8.0.0.");
+            }
+        });
+
+
+
+        pokemonStadium2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpickStarter.setText("Sometimes deemed as a counterpick, with Small Battlefield replacing it as a starter, and other times excluded in favor of the original Pokémon Stadium in certain regions of the world.");
+            }
+        });
+
+
+        kalosPokemonLeague.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpickStarter.setText("Walls located on the ledges of the stage; overall rather large blast zones.");
+            }
+        });
+
+
+        hollowBastion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpickStarter.setText("Stage layout is static and laid out identically to Smashville with hazards off.");
+            }
+        });
+
+
+        yoshisStory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterpick.setText("Features slopes on the ledges; blast zones are rather close to the main stage. Also features a low ceiling, which can lead to early kills. Making this stage banned in some tournaments.");
+
+            }
+        });
 
     }
-
-
-    @Override
-    public void onClick(View view) {
-
-    }
-
 
 }
